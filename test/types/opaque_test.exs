8626d8c423dbaaca7bce3b911207528309a75a75
monolith <monolithic>
2020-02-06 17:29:19 -0600

    Added tests to cover building and encoding errors

--- test/types/opaque_test.exs
+++ test/types/opaque_test.exs
@@ -19,4 +19,29 @@ defmodule XDR.Type.OpaqueTest do
     assert {:error, error} = XDR.build_value(type, "1234")
     assert error.message == "value must be 3 bytes"
   end
+
+  test "errors on unreasonable length", %{len3: _type} do
+    too_long = 1 + XDR.Size.max()
+    too_short = -1 * XDR.Size.max()
+
+    assert_raise XDR.Error, "length value must larger than 0 and smaller than 4294967295", fn ->
+      XDR.build_type(Opaque, too_long)
+    end
+
+    assert_raise XDR.Error, "length value must larger than 0 and smaller than 4294967295", fn ->
+      XDR.build_type(Opaque, too_short)
+    end
+  end
+
+  test "errors on incorrect type", %{len3: type} do
+    assert_raise XDR.Error, "value must be a binary", fn ->
+      XDR.build_value!(type, :not_binary)
+    end
+  end
+
+  test "errors on encoding malformed type", %{len3: type} do
+    assert_raise XDR.Error, "missing or malformed value or length", fn ->
+      XDR.encode!(type)
+    end
+  end
 end
