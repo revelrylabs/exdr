530d345f389eab274a6a6c8caa6862005f7de89b
monolith <monolithic>
2020-02-06 18:48:48 -0600

    Typo correction

--- lib/xdr/types/opaque.ex
+++ lib/xdr/types/opaque.ex
@@ -17,7 +17,7 @@ defmodule XDR.Type.Opaque do
     def build_type(type, length) when is_integer(length) do
       if not XDR.Size.valid?(length) do
         raise XDR.Error,
-              message: "length must be between 0 and #{Size.max()} [0, #{Size.max()}}]",
+              message: "length must be between 0 and #{Size.max()} [0, #{Size.max()}]",
               type: type.type_name
       end
 
